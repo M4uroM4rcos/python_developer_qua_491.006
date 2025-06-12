f5be53a8e7d8b9f898b8052f607ff9602f3bfffd
ead <mauro.marcos20@hotmail.com>
2025-06-11 21:37:33 -0300

    Segundo commit do dia 11/06/2025

---
+++ projetos_console/12_funcoes _de_entrada/main.py
@@ -0,0 +1,11 @@
+nome =  input("Informe seu nome: ").title()
+print(f"Seu nome é: {nome}")
+
+texto = input("Digite uma frase: ").capitalize()
+print(f"frase: {texto}")
+
+minuscula = input("digite um texto em caixa baixa: ").upper()
+print(f"Texto em maiúsculo: {minuscula}.")
+
+maiuscula = input("digite um texto em caixa baixa: ").lower()
+print(f"Texto em minúsculo: {maiuscula}.")
