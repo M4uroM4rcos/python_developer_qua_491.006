636a3f937014b11927de4636f11ff24bc0932940
ead <mauro.marcos20@hotmail.com>
2025-07-01 21:34:51 -0300

    Aula 01/07/2025, manipulao de arquivos

---
+++ manipulacao_arquivo/45_manipulacao_arquivo_parte_04/main.py
@@ -0,0 +1,13 @@
+try:
+    arquivo = input("Informe o nome do arquivo (sem extensão): ").strip().lower()
+    with open(f"{arquivo}.txt", "r", encoding="utf-8") as f:
+        texto = f.read()
+    print(texto)
+
+    novo_texto = input("Digite o texto:\n")
+
+    with open(f"{arquivo}.txt", "w", encoding="utf-8") as f:
+        f.write(novo_texto)
+    print("Arquivo alterado com sucesso.")
+except Exception as e:
+    print(f"Não foi possível a tualizar o arquivo. {e}.")
